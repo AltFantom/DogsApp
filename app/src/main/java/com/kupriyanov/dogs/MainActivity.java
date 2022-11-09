a966fdb49e2552ca3a6d08a8dcad565bf22522f8
AltF4ntom <AltF4ntom@yandex.ru>
2022-11-10 02:50:15 +0300

    Initial commit

---
+++ app/src/main/java/com/kupriyanov/dogs/MainActivity.java
@@ -0,0 +1,54 @@
+package com.kupriyanov.dogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadImage();
+    }
+
+    private void loadImage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    String result;
+                    StringBuilder data = new StringBuilder();
+                    do {
+                        result = bufferedReader.readLine();
+                        if (result != null) {
+                            data.append(result);
+                        }
+                    }while (result != null);
+
+                    Log.d("MainActivity", data.toString());
+                } catch (Exception e) {
+                    Log.d("MainActivity", e.toString());
+                }
+            }
+        }).start();
+
+    }
+}
