805ed3ecf349cd10918cdc2072e5bb6ba3a7b390
AltF4ntom <AltF4ntom@yandex.ru>
2022-11-10 07:35:19 +0300

    Retrofit

---
+++ app/src/main/java/com/kupriyanov/dogs/ApiService.java
@@ -0,0 +1,10 @@
+package com.kupriyanov.dogs;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET("image/random")
+    Single<DogImage> loadDogImage();
+}
